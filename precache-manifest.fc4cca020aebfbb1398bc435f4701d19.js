7df3c0d1f36fa1f1aca64b1ce051f7a2d6207f05
Manjot Singh <manjotsingh16july@gmail.com>
2020-10-22 14:42:27 +0530

    Updates

---
+++ precache-manifest.fc4cca020aebfbb1398bc435f4701d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edf2a781081411aa1c643e5f4d4abcd",
+    "url": "/collegdunia-frontend-task/index.html"
+  },
+  {
+    "revision": "14ca9c87f1c82bc87a49",
+    "url": "/collegdunia-frontend-task/static/css/main.93b64508.chunk.css"
+  },
+  {
+    "revision": "74b3d1d5df09ecfe3b8e",
+    "url": "/collegdunia-frontend-task/static/js/2.e126c676.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/collegdunia-frontend-task/static/js/2.e126c676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ca9c87f1c82bc87a49",
+    "url": "/collegdunia-frontend-task/static/js/main.0b3c4ae5.chunk.js"
+  },
+  {
+    "revision": "9493716416fd06a55ed8",
+    "url": "/collegdunia-frontend-task/static/js/runtime-main.328ad97c.js"
+  }
+]);
